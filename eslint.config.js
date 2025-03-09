52c1e881f784d9d5bba021f60e884634ad606d8e
Walmyr <wlsf82@gmail.com>
2025-03-10 00:10:56 +0100

    Create sample project

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import pluginCypress from 'eslint-plugin-cypress/flat'
 
 export default [
   { ignores: ['dist'] },
@@ -30,4 +31,5 @@ export default [
       ],
     },
   },
+  pluginCypress.configs.recommended,
 ]
