9f78e8f932e44b054d1ed92967ecc4c8418e3c95
Walmyr <wlsf82@gmail.com>
2025-03-12 20:37:00 +0100

    "Refactor": style the Pagination component

--- src/components/Pagination/index.jsx
+++ src/components/Pagination/index.jsx
@@ -1,12 +1,14 @@
+import './Pagination.css'
+
 import Button from '../Button'
 
 const Pagination = ({ currentPage }) => {
   return (
-    <>
+    <div className='pagination'>
       <Button label='Prev' disabled={currentPage === 1} />
       <span>{`Page ${currentPage} of 2`}</span>
       <Button label='Next' />
-    </>
+    </div>
   )
 }
 
