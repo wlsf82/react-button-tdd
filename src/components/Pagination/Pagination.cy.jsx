220ea926d6301c4c0bf84bfd2b90eca0b2e8511b
Walmyr <wlsf82@gmail.com>
2025-03-12 20:39:36 +0100

    Red: add test for disabled pagination next button

--- src/components/Pagination/Pagination.cy.jsx
+++ src/components/Pagination/Pagination.cy.jsx
@@ -8,4 +8,12 @@ describe('<Pagination />', () => {
     cy.contains('button', 'Next').should('be.enabled')
     cy.contains('span', 'Page 1 of 2').should('be.visible')
   })
+
+  it('renders in the last page (Next button disabled)', () => {
+    cy.mount(<Pagination currentPage={2} totalPages={2} />)
+
+    cy.contains('button', 'Prev').should('be.enabled')
+    cy.contains('button', 'Next').should('be.disabled')
+    cy.contains('span', 'Page 2 of 2').should('be.visible')
+  })
 })
