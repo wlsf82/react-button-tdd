ae9fc2f2320c4c943fa08225cf8f49a2b8ab88a0
Walmyr <wlsf82@gmail.com>
2025-03-12 20:31:16 +0100

    Red: add first test for the pagination component

--- src/components/Pagination/Pagination.cy.jsx
+++ src/components/Pagination/Pagination.cy.jsx
@@ -1,3 +1,11 @@
+import Pagination from '.'
+
 describe('<Pagination />', () => {
-  
+  it('renders in the first page (Prev button disabled)', () => {
+    cy.mount(<Pagination currentPage={1} />)
+
+    cy.contains('button', 'Prev').should('be.disabled')
+    cy.contains('button', 'Next').should('be.enabled')
+    cy.contains('span', 'Page 1 of 2').should('be.visible')
+  })
 })
