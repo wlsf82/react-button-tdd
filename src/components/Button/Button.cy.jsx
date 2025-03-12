2fd675e1f3fc4e59ba39ba261ab7d4c726c34778
Walmyr <wlsf82@gmail.com>
2025-03-12 20:23:36 +0100

    Red: add the next button component test

--- src/components/Button/Button.cy.jsx
+++ src/components/Button/Button.cy.jsx
@@ -1,3 +1,5 @@
+import { Send } from 'lucide-react';
+
 import Button from './'
 
 describe('<Button />', () => {
@@ -7,5 +9,17 @@ describe('<Button />', () => {
     cy.contains('button', 'Save')
       .should('be.visible')
       .and('be.enabled')
+  })
+
+  it('renders a send button with a paper airplane icon', () => {
+    cy.mount(<Button label='Send' icon={<Send />} />)
+
+    cy.contains('button', 'Send')
+      .should('be.visible')
+      .and('be.enabled')
+      .as('sendButton')
+    cy.get('@sendButton')
+      .find('svg.lucide-send')
+      .should('exist')
   })
 })
