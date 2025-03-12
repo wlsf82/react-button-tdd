5a934b05e1ad3306685d5aa5ec1f9884f7250726
Walmyr <wlsf82@gmail.com>
2025-03-12 20:17:05 +0100

    Red: add first button component test

--- src/components/Button/Button.cy.jsx
+++ src/components/Button/Button.cy.jsx
@@ -1,3 +1,11 @@
+import Button from './'
+
 describe('<Button />', () => {
-  
+  it('renders a save button', () => {
+    cy.mount(<Button label='Save' />)
+
+    cy.contains('button', 'Save')
+      .should('be.visible')
+      .and('be.enabled')
+  })
 })
