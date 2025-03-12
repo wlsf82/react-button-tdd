1bbcc7233de148f754950ba82cd14f6f5d6634e2
Walmyr <wlsf82@gmail.com>
2025-03-12 20:45:39 +0100

    Green: add test for the middle page

--- src/components/Pagination/Pagination.cy.jsx
+++ src/components/Pagination/Pagination.cy.jsx
@@ -24,4 +24,12 @@ describe('<Pagination />', () => {
     cy.contains('button', 'Next').should('be.disabled')
     cy.contains('span', 'Page 1 of 1').should('be.visible')
   })
+
+  it('renders in the middle page (Prev and Next buttons enabled)', () => {
+    cy.mount(<Pagination currentPage={2} totalPages={3} />)
+
+    cy.contains('button', 'Prev').should('be.enabled')
+    cy.contains('button', 'Next').should('be.enabled')
+    cy.contains('span', 'Page 2 of 3').should('be.visible')
+  })
 })
